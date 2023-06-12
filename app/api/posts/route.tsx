b647f9ff4eb98794277f8d2a779d444d28396200
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-06-12 22:16:15 +0300

    Обработка GET запроса с query-параметрами.

--- app/api/posts/route.tsx
+++ app/api/posts/route.tsx
@@ -4,6 +4,22 @@ import {posts} from "@/app/api/posts/posts";
 //http://localhost:3000/api/posts
 export async function GET(request: Request) {
     // return NextResponse.json({message: 'hello'})
+
+    //Получение параметров запроса в серверном компоненте
+    const urlSearchParams = request.nextUrl.searchParams
+    console.log(urlSearchParams.get("a"), urlSearchParams.get('b'))
+
+    const {searchParams} = new URL(request.url)
+    const query = searchParams.get("a")
+    console.log(query)
+
+    if (query) {
+        let currentPosts = posts.filter(post => {
+            return post.title.toLowerCase().includes(query?.toLowerCase())
+        })
+        console.log(currentPosts)
+        return NextResponse.json(currentPosts)
+    }
     return NextResponse.json(posts)
 }
 
