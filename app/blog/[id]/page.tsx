49b4bce1731dabde4c50ffab0479dc6fbf6e8522
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-06-12 09:41:03 +0300

    Асинхронно получение данных из API и генерация статических страниц на основе них на серевере. Асинхронные серверные компоненты. Кеширование. Ревалидация. SEO.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,15 @@
 import React, {JSX} from "react";
 import {Metadata} from "next";
+import {fetch} from "next/dist/compiled/@edge-runtime/primitives";
+import {useRouter} from "next/router";
+import {useSearchParams} from "next/navigation";
+
+interface IPost {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
 
 type Props = {
     params: {
@@ -8,14 +18,31 @@ type Props = {
 }
 
 export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
+    const post = await getData(id)
     return {
-        title: `Title ${id}`,
-        description: `Description ${id}`
+        title: `${post.title}`,
+        description: `Description ${post.body}`
     }
 }
 
-export default function Posts({params: {id}}: Props): JSX.Element {
+async function getData(id: string): Promise<IPost> {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        next: {
+            revalidate: 60
+        }
+    })
+
+    return response.json()
+}
+
+export default async function Posts({params: {id}}: Props) {
+    const post = await getData(id)
     return (
-        <h1>Post page №{id}</h1>
+        <div>
+            <h1>Post page №{id}</h1>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p>{post.userId}</p>
+        </div>
     )
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,38 @@
-export default () => {
+import {Metadata} from "next";
+import Link from "next/link";
+
+interface IPost {
+    id: number,
+    title: string
+}
+
+export const metadata: Metadata = {
+    title: "Blog Start Page",
+    description: "Blog Start Page description"
+}
+
+async function getData(): Promise<IPost[]> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: {
+            revalidate: 60  //периодичность обновления кеш
+        }
+    })
+    return response.json()
+}
+
+export default async () => {
+    const posts = await getData()
     return (
-        <h1>Blog</h1>
+        <>
+            <h1>Blog</h1>
+            <ul>
+                {posts.map((post: IPost) => (
+                        <li key={post.id}>
+                            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+                        </li>
+                    )
+                )}
+            </ul>
+        </>
     )
 }
